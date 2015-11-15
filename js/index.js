e14c8fda57a36f407d524a8fe641aee3eed9ccb1
yhsunshining <yhsunshining@gmail.com>
2015-11-15 15:56:13 +0800

    init

---
+++ js/index.js
@@ -0,0 +1,18 @@
+define(function (require) {
+    var $ = require('./util');
+    window.$=$;
+    var flick = "http://api.flickr.com/services/feeds/photos_public.gne?tags=cat&tagmode=any&format=json&jsoncallback=onsuccess"
+    var resutl="js/result.js"
+    $.ajax(flick,{
+    	data:{
+    		x:1,
+    		string:"string"
+    	},
+    	type:"jsonp",
+    	onsuccess:function(res){
+//  		res=JSON.parse(res);
+    		console.log(res);
+    		delete window.onsuccess;
+    	}
+    });
+});
---
+++ js/util.js
@@ -0,0 +1,59 @@
+define(function (require, exports) {
+	'use strict'
+	exports.ajax = function (url,options) {
+		var data = options.data;
+		var successfunc = options.onsuccess;
+		var errorfunc = options.onerror;
+		var type = options.type || "GET";
+		var timeout = parseInt(options.timeout) || 600;
+		var timeoutFlag = false;
+		type = type.toUpperCase();
+		if(type==='JSONP'){
+			var num = this.count++;
+			var name = "callback"+num;
+			var script = document.createElement("script");
+        	try{
+        		window.onsuccess=successfunc;
+        	}
+        	finally{
+//      		script.parentNode.removeChild(script);
+        	}
+        	script.setAttribute("src",url);
+        	document.getElementsByTagName("body")[0].appendChild(script);
+		}
+		else {
+			var xhr = new XMLHttpRequest();
+			var timer = setTimeout(function(){
+				timeoutFlag = true;
+				xhr.abort();
+			},timeout)
+			xhr.open(type,url,true);
+			
+			if(type ==="GET"){
+				xhr.send(null);
+			}
+			else {
+				xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+				xhr.send(JSON.stringify(data));
+			}
+			xhr.onreadystatechange = function() {
+				if (xhr.readyState === 4 && !timeoutFlag) {
+					clearTimeout(timer);
+					if (xhr.status === 200) {
+						if (successfunc) {
+	//						if(xhr.getResponseHeader("Content-Type").match(/^text/)){
+								successfunc(xhr.responseText);
+	//						}
+						}
+					} else {
+						if (errorfunc) {
+							errorfunc();
+						}
+					}
+				}
+			};
+		}
+		
+	};
+	exports.ajax.count=0;
+});
